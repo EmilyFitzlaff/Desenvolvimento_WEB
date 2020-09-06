41d986c0fe37c9a4bf176f5ee9bb98ea3c8ce78c
Emily Fitzlaff <fitzlaff.emily@gmail.com>
2020-09-06 11:54:08 -0300

    Estruturação página principal

--- Aula 03 - CSS/Site pessoal/index.php
+++ Aula 03 - CSS/Site pessoal/index.php
@@ -7,6 +7,16 @@
 
     <body>
         <?php include_once('header.php'); ?>
+        <main>
+            <h2 class="titulo">Lorem Ipsum</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sed molestie tellus. Nam gravida, quam at gravida molestie, neque ipsum fringilla eros, quis varius risus erat a diam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Suspendisse potenti. Maecenas commodo justo quis ipsum facilisis, ornare pulvinar metus elementum. Sed mollis maximus rhoncus. Maecenas tempor ipsum in lobortis mollis.</p>
+
+            <p>Phasellus faucibus arcu nisi, a auctor orci pharetra in. Sed vulputate, mauris at dapibus faucibus, mauris orci finibus ligula, in vehicula dolor sapien in elit. Fusce semper ante vel elementum lobortis. Sed suscipit sem turpis, tempus aliquam ante auctor eget. Morbi tristique diam arcu, et feugiat orci laoreet eu. Morbi ut molestie libero, ut fermentum justo. Curabitur mi est, feugiat non sodales non, fringilla id erat. Aliquam nec metus risus. Donec suscipit, eros nec pellentesque sollicitudin, urna erat dignissim velit, sit amet euismod elit felis nec tortor. Curabitur sed bibendum nulla. Donec nec gravida risus. Aliquam ut odio in purus cursus sagittis vitae eleifend sapien. Sed ut purus vitae ipsum imperdiet varius. Aliquam semper hendrerit ligula id luctus. Nunc dapibus sit amet massa et vulputate.</p>
+
+            <p>Mauris ac justo nibh. Vestibulum gravida laoreet ex, eget tristique sapien venenatis quis. In gravida, est vitae pharetra convallis, nibh tortor lobortis ipsum, eget lobortis turpis purus vel nibh. Nulla facilisis risus non nisi finibus, ac egestas diam congue. Vivamus vulputate odio id mollis laoreet. Sed malesuada, massa quis consectetur porta, nibh neque posuere ante, sit amet vulputate ligula purus pharetra justo. Vestibulum blandit enim tincidunt dui pretium finibus. Donec facilisis arcu vitae ligula condimentum faucibus. Etiam hendrerit, enim dignissim maximus blandit, tortor sem vehicula tortor, ut blandit ligula orci vel nisl. Donec hendrerit placerat augue ut laoreet. Curabitur quis malesuada risus, et sodales tortor. Ut est dolor, varius sit amet aliquam nec, placerat vitae nisi.</p>
+
+            <p>Morbi in pulvinar libero. Maecenas sed ipsum lobortis, consectetur magna ut, accumsan velit. Curabitur tempus diam et sem tempor, ac fermentum ipsum dictum. Vestibulum posuere lacus libero, quis porta orci molestie eu. Nunc a turpis nec risus varius aliquet vitae a est. Etiam suscipit sodales nisi, sed commodo lectus vulputate in. Maecenas vel mollis mi. Maecenas faucibus, elit at ullamcorper viverra, orci nunc maximus augue, in feugiat velit turpis id nisl. Maecenas tempor tortor non urna aliquam, eget aliquam dolor sodales. Quisque fermentum elementum libero a condimentum. Mauris auctor ipsum non enim tincidunt, in dignissim erat molestie. Pellentesque eleifend augue vitae erat tincidunt, eget vehicula magna commodo. Maecenas eget justo justo. Integer et urna volutpat, finibus sapien et, semper dui.</p>
+        </main>
         <?php include_once('footer.php'); ?>
     </body>
 </html>
