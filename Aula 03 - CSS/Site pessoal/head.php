20dae87aaf69d5f434b4977f2acde11c0cc68ee1
Emily Fitzlaff <fitzlaff.emily@gmail.com>
2020-09-07 09:16:30 -0300

    Tabela Info Pessoais

--- Aula 03 - CSS/Site pessoal/head.php
+++ Aula 03 - CSS/Site pessoal/head.php
@@ -1,3 +1,5 @@
 <title><?php echo $title; ?></title>
 <link rel="stylesheet" href="css/estilo.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<body>
 <meta charset="utf-8">
--- Aula 03 - CSS/Site pessoal/info-pessoais.php
+++ Aula 03 - CSS/Site pessoal/info-pessoais.php
@@ -8,7 +8,49 @@
     <body>
         <?php include_once('header.php'); ?>
         <main>
-        
+            <table border="1">
+                <caption>Informações Pessoais</caption>         
+                <tr>
+                    <td>Nome</td>
+                    <td colspan="3">Emily da Silva Fitzlaff</td>
+                </tr>
+
+                <tr>
+                    <td>Endereço</td>
+                    <td colspan="3">Rua das Flores, nº 0, Bairro Jardim</td>
+                </tr>
+
+                <tr>
+                    <td>Cidade</td>
+                    <td>Rio do Sul</td>
+                    <td>Estado</td>
+                    <td>Santa Catarina</td>
+                </tr>
+
+                <tr>
+                    <td>Telefone</td>
+                    <td colspan="3">00 00000 - 0000</td>
+                </tr>
+
+                <tr>
+                    <td colspan="4">Redes Sociais</td>
+                </tr>
+
+                <tr>
+                    <td>Instagram</td>
+                    <td colspan="3"><a target="_blank" href="https://www.instagram.com/emilyfitzlaff/">@EmilyFitzlaff</a></td>
+                </tr>
+
+                <tr>
+                    <td>Facebook</td>
+                    <td colspan="3"><a target="_blank" href="https://www.facebook.com/emilyfitzlaff/">Emily Fitzlaff</a></td>
+                </tr>
+
+                <tr>
+                    <td>Twitter</td>
+                    <td colspan="3"><a target="_blank" href="https://www.twitter.com/emilyfitzlaff/">@EmilyFitzlaff</a></td>
+                </tr>
+            </table>        
         </main>
         <?php include_once('footer.php'); ?>
     </body>
