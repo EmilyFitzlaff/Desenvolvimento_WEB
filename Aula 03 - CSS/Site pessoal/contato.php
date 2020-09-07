4ef9e908fdcc333cca2302ed94f8b62e127f09e6
Emily Fitzlaff <fitzlaff.emily@gmail.com>
2020-09-07 14:22:03 -0300

    Finalização

--- Aula 03 - CSS/Site pessoal/contato.php
+++ Aula 03 - CSS/Site pessoal/contato.php
@@ -8,7 +8,29 @@
     <body>
         <?php include_once('header.php'); ?>
         <main>
-        
+            <form>
+                <fieldset class="center">
+                    <legend>Contato</legend>
+                    <label for="nome">Nome</label>
+                    <input type="text" id="nome" placeholder="Informe seu nome completo" required>
+
+                    <label for="email">E-mail</label>
+                    <input type="mail" id="email" placeholder="usuario@mail.com" required>
+
+                    <label for="assunto">Escolha o assunto</label>
+                    <select>
+                        <option value="NI" disabled selected class="disabled">Não informado</option>
+                        <option value="sugestao">Sugestão</option>
+                        <option value="elogia">Elogia</option>
+                        <option value="reclamacao">Reclamação</option>
+                    </select>
+
+                    <label for="msg">Mensagem</label>
+                    <textarea id="msg" name="msg" rows="4" cols="50"></textarea>
+                </fiedlset> 
+
+                <button class="enviar">Enviar</button>
+            </form>       
         </main>
         <?php include_once('footer.php'); ?>
     </body>
--- Aula 03 - CSS/Site pessoal/curriculo.php
+++ Aula 03 - CSS/Site pessoal/curriculo.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<?php $title = "Currículos"; ?>
+<?php $title = "Currículo"; ?>
 
     <head>
         <?php include_once('head.php'); ?>
@@ -8,7 +8,95 @@
     <body>
         <?php include_once('header.php'); ?>
         <main>
-        
+            <h2 class="titulo">Currículo</h2>
+
+            <div class="center">
+                <table>
+                    <caption>Formação Acadêmica </caption>         
+                    <tr>
+                        <td colspan="4">Técnico em Informática Integrado ao Ensino Médio</td>
+                    </tr>
+
+                    <tr>
+                        <td class="title-table">Período</td>
+                        <td colspan="3">2014 a 2016</td>
+                    </tr>
+
+                    <tr>
+                        <td class="title-table">Instituição</td>
+                        <td colspan="3">Instituito Federal Catarinense - Campus Rio do Sul</td>
+                    </tr>
+
+                    <tr>
+                        <td colspan="4">Bacharelado em Sistemas de Informação</td>
+                    </tr>
+
+                    <tr>
+                        <td class="title-table">Período</td>
+                        <td colspan="3">2019 a 2022</td>
+                    </tr>
+
+                    <tr>
+                        <td class="title-table">Instituição</td>
+                        <td colspan="3">Centro Universitário para o Desenvolvimento do Alto Vale - UNIDAVI</td>
+                    </tr>
+                </table>
+
+                <br>
+
+                <table>
+                    <caption>Experiências Profissionais</caption>         
+                    <tr>
+                        <td colspan="4">Estágio</td>
+                    </tr>
+
+                    <tr>
+                        <td class="title-table">Período</td>
+                        <td colspan="3">Fev/2019 a Ago/2020</td>
+                    </tr>
+
+                    <tr>
+                        <td class="title-table">Local</td>
+                        <td colspan="3">Centro Universitário para o Desenvolvimento do Alto Vale - UNIDAVI</td>
+                    </tr>
+
+                    <tr>
+                        <td class="title-table">Principais Atividades</td>
+                        <td colspan="3">
+                            <ul>
+                                <li>Reservas de Laboratório</li>
+                                <li>Suporte aos usuários</li>
+                                <li>Manutenção dos Computadores</li>
+                            </ul>   
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td colspan="4">Técnico em Software (Triagem) - Recursos Humanos</td>
+                    </tr>
+
+                    <tr>
+                        <td class="title-table">Período</td>
+                        <td colspan="3">Ago/2019 - Atual</td>
+                    </tr>
+
+                    <tr>
+                        <td class="title-table">Local</td>
+                        <td colspan="3">IPM Sistemas</td>
+                    </tr>
+
+                    <tr>
+                        <td class="title-table">Principais Atividades</td>
+                        <td colspan="3">
+                            <ul>
+                                <li>Suporte aos Técnicos Externos e Residentes do núcleo RH</li>
+                                <li>Triagem dos chamados/atendimentos direcionados ao desenvolvimento do núcleo RH</li>
+                                <li>Auxílio em testes</li>
+                            </ul>   
+                        </td>
+                    </tr>
+                </table>
+            </div>
         </main>
         <?php include_once('footer.php'); ?>
     </body>
