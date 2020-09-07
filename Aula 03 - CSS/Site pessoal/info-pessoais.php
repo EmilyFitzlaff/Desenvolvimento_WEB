b1cbc1da781ce8b798bf4fa782cba00468d885c3
Emily Fitzlaff <fitzlaff.emily@gmail.com>
2020-09-07 09:29:26 -0300

    Alterações tabela

--- Aula 03 - CSS/Site pessoal/info-pessoais.php
+++ Aula 03 - CSS/Site pessoal/info-pessoais.php
@@ -8,49 +8,51 @@
     <body>
         <?php include_once('header.php'); ?>
         <main>
-            <table border="1">
-                <caption>Informações Pessoais</caption>         
-                <tr>
-                    <td>Nome</td>
-                    <td colspan="3">Emily da Silva Fitzlaff</td>
-                </tr>
+            <div class="center">
+                <table>
+                    <caption>Informações Pessoais</caption>         
+                    <tr>
+                        <td class="title-table">Nome</td>
+                        <td colspan="3">Emily da Silva Fitzlaff</td>
+                    </tr>
 
-                <tr>
-                    <td>Endereço</td>
-                    <td colspan="3">Rua das Flores, nº 0, Bairro Jardim</td>
-                </tr>
+                    <tr>
+                        <td class="title-table">Endereço</td>
+                        <td colspan="3">Rua das Flores, nº 0, Bairro Jardim</td>
+                    </tr>
 
-                <tr>
-                    <td>Cidade</td>
-                    <td>Rio do Sul</td>
-                    <td>Estado</td>
-                    <td>Santa Catarina</td>
-                </tr>
+                    <tr>
+                        <td class="title-table">Cidade</td>
+                        <td>Rio do Sul</td>
+                        <td class="title-table">Estado</td>
+                        <td>Santa Catarina</td>
+                    </tr>
 
-                <tr>
-                    <td>Telefone</td>
-                    <td colspan="3">00 00000 - 0000</td>
-                </tr>
+                    <tr>
+                        <td class="title-table">Telefone</td>
+                        <td colspan="3">00 00000 - 0000</td>
+                    </tr>
 
-                <tr>
-                    <td colspan="4">Redes Sociais</td>
-                </tr>
+                    <tr>
+                        <td colspan="4"  class="title-table">Redes Sociais</td>
+                    </tr>
 
-                <tr>
-                    <td>Instagram</td>
-                    <td colspan="3"><a target="_blank" href="https://www.instagram.com/emilyfitzlaff/">@EmilyFitzlaff</a></td>
-                </tr>
+                    <tr>
+                        <td  class="title-table">Instagram</td>
+                        <td colspan="3"><a target="_blank" href="https://www.instagram.com/emilyfitzlaff/">@EmilyFitzlaff</a></td>
+                    </tr>
 
-                <tr>
-                    <td>Facebook</td>
-                    <td colspan="3"><a target="_blank" href="https://www.facebook.com/emilyfitzlaff/">Emily Fitzlaff</a></td>
-                </tr>
+                    <tr>
+                        <td  class="title-table">Facebook</td>
+                        <td colspan="3"><a target="_blank" href="https://www.facebook.com/emilyfitzlaff/">Emily Fitzlaff</a></td>
+                    </tr>
 
-                <tr>
-                    <td>Twitter</td>
-                    <td colspan="3"><a target="_blank" href="https://www.twitter.com/emilyfitzlaff/">@EmilyFitzlaff</a></td>
-                </tr>
-            </table>        
+                    <tr>
+                        <td  class="title-table">Twitter</td>
+                        <td colspan="3"><a target="_blank" href="https://www.twitter.com/emilyfitzlaff/">@EmilyFitzlaff</a></td>
+                    </tr>
+                </table>    
+            </div>    
         </main>
         <?php include_once('footer.php'); ?>
     </body>
