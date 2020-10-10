83f9941edf3bd7a94bbcf537fb038f05352b5373
Emily Fitzlaff <fitzlaff.emily@gmail.com>
2020-10-10 10:40:56 -0300

    Elaboração exercício 01

---
+++ Aula 08 - Introdução PHP/ex01.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"> 
+        <title>Exercício 01</title>        
+    </head>
+
+    <body>
+        <?php
+            $i = 1;
+
+            while ($i <= 50) {
+                echo "Está é a linha " . $i . "<br>";
+
+                $i ++;
+            }
+        ?>
+    </body>
+</html>
