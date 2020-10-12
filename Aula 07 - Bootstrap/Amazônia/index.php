e23285ebcc119ec025847a88fa0b84fd95e0a058
Emily Fitzlaff <fitzlaff.emily@gmail.com>
2020-10-12 13:20:41 -0300

    Elaboração da página inicial

--- Aula 07 - Bootstrap/Amazônia/index.php
+++ Aula 07 - Bootstrap/Amazônia/index.php
@@ -8,9 +8,18 @@
     </head>
     
     <body>
-        <div class="container">
-            <?php include_once('menu.php'); ?>
-
+        <?php include_once('menu.php'); ?>
+        <div class="container top-margin">           
+            <h1>Amazônia</h1>
+            <p class="font-italic">Origem: Wikipédia, a enciclopédia livre.</p>
+            <p class="paragrafo">Amazônia (português brasileiro) ou Amazónia (português europeu) (também chamada de Floresta Amazônica, Selva Amazônica, Floresta Equatorial da Amazônia, Floresta Pluvial ou Hileia Amazônica) é uma floresta latifoliada úmida que cobre a maior parte da Bacia Amazônica da América do Sul. Esta bacia abrange 7 milhões de quilômetros quadrados, dos quais 5 milhões e meio de quilômetros quadrados são cobertos pela floresta tropical.</p>
+            <p class="paragrafo">Esta região inclui territórios pertencentes a nove nações. A maioria das florestas está contida dentro do Brasil, com 60% da floresta, seguida pelo Peru com 13% e com partes menores na Colômbia, Venezuela, Equador, Bolívia, Guiana, Suriname e França (Guiana Francesa). Estados ou departamentos de quatro nações vizinhas do Brasil têm o nome de Amazonas por isso. A Amazônia representa mais da metade das florestas tropicais remanescentes no planeta e compreende a maior biodiversidade em uma floresta tropical no mundo. É um dos seis grandes biomas brasileiros.</p>
+            <p class="paragrafo">No Brasil, para efeitos de governo e economia, a Amazônia é delimitada por uma área chamada "Amazônia Legal" definida a partir da criação da Superintendência do Desenvolvimento da Amazônia (SUDAM), em 1966. É chamado também de Amazônia o bioma que, no Brasil, ocupa 49,29% do território e abrange três das cinco divisões regionais do país (Norte, Nordeste e Centro-Oeste), sendo o maior bioma terrestre do país.</p>
+            <p class="paragrafo">Uma área de seis milhões de hectares no centro de sua bacia hidrográfica, incluindo o Parque Nacional do Jaú, foi considerada pela Organização das Nações Unidas para a Educação, a Ciência e a Cultura, em 2000 (com extensão em 2003), Patrimônio da Humanidade. A Amazônia também foi pré-selecionada em 2008 como candidata a uma das sete maravilhas naturais do mundo, sendo classificada em primeiro lugar no Grupo E, a categoria para as florestas, parques nacionais e reservas naturais, em fevereiro de 2009.</p>
+            <figure>
+                <img src="img/territorio.jpg"  class="rounded mx-auto d-block" >
+                <figcaption class="figure-caption text-center">Mapa da ecorregião amazônica definida pelo WWF.</figcaption>
+            </figure>
             <?php include_once('footer.php'); ?>
         </div>
     </body>
--- Aula 07 - Bootstrap/Amazônia/menu.php
+++ Aula 07 - Bootstrap/Amazônia/menu.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light bg-green">
+<nav class="navbar navbar-expand-lg navbar-light bg-light bg-green" style="background-color: #519548;">
     <a class="navbar-brand" href="index.php">Tudo Sobre a Amazônia</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
