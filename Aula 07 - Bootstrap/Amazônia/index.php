f43802e0051564aa4b1c6d1571b5e5c9fae7b16f
Emily Fitzlaff <fitzlaff.emily@gmail.com>
2020-10-12 10:03:28 -0300

    Trabalho Amazônio - arquivos iniciais

---
+++ Aula 07 - Bootstrap/Amazônia/index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="pt-br">
+    <head>
+        <?php 
+            $title = "Amazônia - Página Principal";
+            include_once('head.php');
+        ?>
+    </head>
+    
+    <body>
+
+
+        <?php include_once('footer.php'); ?>
+    </body>
+</html>
