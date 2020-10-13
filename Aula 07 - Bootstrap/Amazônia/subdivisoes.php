044dd84ac35e2a65ad894bb445194b3581bcb742
Emily Fitzlaff <fitzlaff.emily@gmail.com>
2020-10-12 21:29:05 -0300

    Página Subdivisões

---
+++ Aula 07 - Bootstrap/Amazônia/subdivisoes.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="pt-br">
+    <head>
+        <?php 
+            $title = "Amazônia - Subdivisões";
+            include_once('head.php');
+        ?>
+    </head>
+    
+    <body>
+        <?php include_once('menu.php'); ?>
+        <div class="container top-margin">           
+            <h1>Subdivisões</h1>
+            <p class="paragrafo">A conceito de Amazônia pode variar dependendo do ponto de vista (fisiográfico, geomorfológico, biogeográfico, político, de planejamento territorial, etc.), da mesma maneira, variam sua extensão, suas subdivisões e a terminologia utilizada.</p>
+
+            <h3>Em biogegrafia</h3>
+            <p>Divisão fitogeográfica de Ducke e Black (1954):</p>
+            <ul>
+                <li>Setor Atlântico</li>
+                <li>Setor Nordeste</li>
+                <li>Setor Sudeste</li>
+                <li>Setor Norte</li>
+                <li>Setor Sul</li>
+            </ul>
+
+            <p>Divisão fitogeográfica proposta por Rizzini (1963):</p>
+            <ul>
+                <li>Província Amazônica (inclui Floresta amazônica e Campos do Alto Rio Branco)</li>
+                <li>Subprovíncia do Alto Rio Branco</li>
+                <li>Subprovíncia do Jari-Trombetas</li>
+                <li>Subprovíncia do Rio Negro</li>
+                <li>Subprovíncia da Planície Terciária</li>
+            </ul>
+
+            <p>Esquema biogeográfico, por Morrone (2001):</p>
+            <ul>
+                <li>Região Neotropical</li>
+                <li>Sub-região Amazônica</li>
+                <li>Províncias: Napo, Imerí, Guiana, Guiana Húmida, Roraima, Amapá, Várzea, Ucayali, Madeira, Tapajós-Xingú, Pará, Pantanal, Yungas</li>
+            </ul>
+            <h3>Em ecologia da vegetação</h3>
+            <p class="paragrafo">Em termos de vegetação, Martius (1824) já citava a distinção, na região amazônica, entre "mato virgem" (florestas), "gabós" (igapós) e "campinas".</p>
+            <p>Spruce (1908) elencou os seguintes tipos de vegetação na Amazônia:</p>
+            <ul>
+                <li>mato (ou floresta virgem, de terra firme, caa-guaçú, monte alto),</li>
+                <li>matinho (capoeiras, florestas recentes, rastrojos),</li>
+                <li>floresta de gapó (ou ripárias, igapós, rebalsas),</li>
+                <li>florestas baixas (ou brancas, caa-tingas, monte bajo),</li>
+                <li>savanas (ou campos),</li>
+                <li>e caatinga-gapó.</li>
+            </ul>
+            <?php include_once('footer.php'); ?>
+        </div>
+    </body>
+</html>
