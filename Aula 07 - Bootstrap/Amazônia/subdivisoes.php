d0f9943f0afc121a1218e8910c35d46e8ef6b713
Emily Fitzlaff <fitzlaff.emily@gmail.com>
2020-10-12 21:54:23 -0300

    Upload imagens, término página subdivisoes.php

--- Aula 07 - Bootstrap/Amazônia/subdivisoes.php
+++ Aula 07 - Bootstrap/Amazônia/subdivisoes.php
@@ -9,46 +9,105 @@
     
     <body>
         <?php include_once('menu.php'); ?>
-        <div class="container top-margin">           
-            <h1>Subdivisões</h1>
-            <p class="paragrafo">A conceito de Amazônia pode variar dependendo do ponto de vista (fisiográfico, geomorfológico, biogeográfico, político, de planejamento territorial, etc.), da mesma maneira, variam sua extensão, suas subdivisões e a terminologia utilizada.</p>
+        <div class="container top-margin"> 
+            <div class="row">
+                <div class="col-8">
+                    <h1>Subdivisões</h1>
+                    <p class="font-italic">Origem: Wikipédia, a enciclopédia livre.</p>
+                    <p class="paragrafo">A conceito de Amazônia pode variar dependendo do ponto de vista (fisiográfico, geomorfológico, biogeográfico, político, de planejamento territorial, etc.), da mesma maneira, variam sua extensão, suas subdivisões e a terminologia utilizada.</p>
 
-            <h3>Em biogegrafia</h3>
-            <p>Divisão fitogeográfica de Ducke e Black (1954):</p>
-            <ul>
-                <li>Setor Atlântico</li>
-                <li>Setor Nordeste</li>
-                <li>Setor Sudeste</li>
-                <li>Setor Norte</li>
-                <li>Setor Sul</li>
-            </ul>
+                    <h3>Em biogegrafia</h3>
+                    <p>Divisão fitogeográfica de Ducke e Black (1954):</p>
+                    <ul>
+                        <li>Setor Atlântico</li>
+                        <li>Setor Nordeste</li>
+                        <li>Setor Sudeste</li>
+                        <li>Setor Norte</li>
+                        <li>Setor Sul</li>
+                    </ul>
 
-            <p>Divisão fitogeográfica proposta por Rizzini (1963):</p>
-            <ul>
-                <li>Província Amazônica (inclui Floresta amazônica e Campos do Alto Rio Branco)</li>
-                <li>Subprovíncia do Alto Rio Branco</li>
-                <li>Subprovíncia do Jari-Trombetas</li>
-                <li>Subprovíncia do Rio Negro</li>
-                <li>Subprovíncia da Planície Terciária</li>
-            </ul>
+                    <p>Divisão fitogeográfica proposta por Rizzini (1963):</p>
+                    <ul>
+                        <li>Província Amazônica (inclui Floresta amazônica e Campos do Alto Rio Branco)</li>
+                        <li>Subprovíncia do Alto Rio Branco</li>
+                        <li>Subprovíncia do Jari-Trombetas</li>
+                        <li>Subprovíncia do Rio Negro</li>
+                        <li>Subprovíncia da Planície Terciária</li>
+                    </ul>
 
-            <p>Esquema biogeográfico, por Morrone (2001):</p>
-            <ul>
-                <li>Região Neotropical</li>
-                <li>Sub-região Amazônica</li>
-                <li>Províncias: Napo, Imerí, Guiana, Guiana Húmida, Roraima, Amapá, Várzea, Ucayali, Madeira, Tapajós-Xingú, Pará, Pantanal, Yungas</li>
-            </ul>
-            <h3>Em ecologia da vegetação</h3>
-            <p class="paragrafo">Em termos de vegetação, Martius (1824) já citava a distinção, na região amazônica, entre "mato virgem" (florestas), "gabós" (igapós) e "campinas".</p>
-            <p>Spruce (1908) elencou os seguintes tipos de vegetação na Amazônia:</p>
-            <ul>
-                <li>mato (ou floresta virgem, de terra firme, caa-guaçú, monte alto),</li>
-                <li>matinho (capoeiras, florestas recentes, rastrojos),</li>
-                <li>floresta de gapó (ou ripárias, igapós, rebalsas),</li>
-                <li>florestas baixas (ou brancas, caa-tingas, monte bajo),</li>
-                <li>savanas (ou campos),</li>
-                <li>e caatinga-gapó.</li>
-            </ul>
+                    <p>Esquema biogeográfico, por Morrone (2001):</p>
+                    <ul>
+                        <li>Região Neotropical</li>
+                        <li>Sub-região Amazônica</li>
+                        <li>Províncias: Napo, Imerí, Guiana, Guiana Húmida, Roraima, Amapá, Várzea, Ucayali, Madeira, Tapajós-Xingú, Pará, Pantanal, Yungas</li>
+                    </ul>
+                    <h3>Em ecologia da vegetação</h3>
+                    <p class="paragrafo">Em termos de vegetação, Martius (1824) já citava a distinção, na região amazônica, entre "mato virgem" (florestas), "gabós" (igapós) e "campinas".</p>
+                    <p>Spruce (1908) elencou os seguintes tipos de vegetação na Amazônia:</p>
+                    <ul>
+                        <li>mato (ou floresta virgem, de terra firme, caa-guaçú, monte alto),</li>
+                        <li>matinho (capoeiras, florestas recentes, rastrojos),</li>
+                        <li>floresta de gapó (ou ripárias, igapós, rebalsas),</li>
+                        <li>florestas baixas (ou brancas, caa-tingas, monte bajo),</li>
+                        <li>savanas (ou campos),</li>
+                        <li>e caatinga-gapó.</li>
+                    </ul>
+
+                    <p>Ducke e Black (1954) usaram as seguintes categorias:</p>
+                    <ul>
+                        <li>Florestas</li>
+                        <li>Mata da várzea</li>
+                        <li>Mata da terra firme</li>
+                        <li>Áreas abertas ou savanas</li>
+                        <li>Campos da várzea</li>
+                        <li>Campos firmes</li>
+                        <li>Campinas</li>
+                        <li>Campinaranas (transição entre campos e florestas)</li>
+                        <li>Catingas do alto rio Negro</li>
+                    </ul>
+
+                    <p>Braga (1979) adotou um esquema e nomenclatura ligeiramente diferentes:</p>
+                    <ul>
+                        <li>Floresta de terra firme</li>
+                        <li>Floresta de várzea</li>
+                        <li>Floresta de igapó</li>
+                        <li>Manguezais</li>
+                        <li>Campos de várzea</li>
+                        <li>Campos de terra firme</li>
+                        <li>Campinas</li>
+                        <li>Vegetação serrana</li>
+                        <li>Vegetação de restinga</li>
+                    </ul>
+                </div>
+                <div class="col-4">    
+                    <figure>
+                        <img src="img/Anavilhanas.jpg"  class="rounded mx-auto d-block" >
+                        <figcaption class="figure-caption text-center">Parque Nacional de Anavilhanas, no Amazonas</figcaption>
+                    </figure>
+                    <br>
+                    <figure>
+                        <img src="img/CachoeiraAraras.jpg"  class="rounded mx-auto d-block" >
+                        <figcaption class="figure-caption text-center">Parque Nacional da Amazônia, no Pará</figcaption>
+                    </figure>
+                    <br>
+                    <figure>
+                        <img src="img/SaltoAugusto.jpg"  class="rounded mx-auto d-block" >
+                        <figcaption class="figure-caption text-center">Parque Nacional do Juruena, no Mato Grosso</figcaption>
+                    </figure>
+                    <br>
+                    <figure>
+                        <img src="img/SaltoAugusto.jpg"  class="rounded mx-auto d-block" >
+                        <figcaption class="figure-caption text-center">Parque Nacional do Pico da Neblina, Amazonas</figcaption>
+                    </figure>
+                    <br>
+                    <figure>
+                        <img src="img/RioNegro.jpg"  class="rounded mx-auto d-block" >
+                        <figcaption class="figure-caption text-center">Parque Estadual Rio Negro Setor Sul, Amazonas</figcaption>
+                    </figure>
+
+                    
+                </div>
+            </div>          
             <?php include_once('footer.php'); ?>
         </div>
     </body>
