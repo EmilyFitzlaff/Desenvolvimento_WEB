27545b226213f88711fb663cd460106583722799
Emily Fitzlaff <fitzlaff.emily@gmail.com>
2020-11-21 11:11:52 -0300

    Elaboração inicial do projeto;

---
+++ Northwind/index.php
@@ -0,0 +1,13 @@
+<?php
+    $title = "Página Inicial";
+
+    require_once ('config/parameters.php');
+    require_once LAYOUTS."/header.php";
+    require_once CLASSES."/class_connection.php";
+?>
+
+<body>
+    <?php 
+            
+    ?>        
+</body>
