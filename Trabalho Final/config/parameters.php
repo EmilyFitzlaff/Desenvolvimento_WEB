59f86a343e97a4efc5a15ba6a888accd3a2661f3
Emily Fitzlaff <fitzlaff.emily@gmail.com>
2020-11-27 17:53:38 -0300

    Criação dos arquivos para alteração

--- Trabalho Final/config/parameters.php
+++
@@ -1,9 +0,0 @@
-<?php
-    define("DIR_PADRAO", dirname(__DIR__));
-    define("CONFIG",     dirname(__DIR__) ."/config/");
-    define("CSS",        dirname(__DIR__) ."/css/");
-    define("INCLUDES",   dirname(__DIR__) ."/includes/");
-    define("CLASSE",     dirname(__DIR__) ."/class/");
-    define("BD",         dirname(__DIR__) ."/bd/");
-    define("CADASTROS",  dirname(__DIR__) ."/cadastros/");
-?>
--- Trabalho Final/includes/footer.php
+++
@@ -1,3 +0,0 @@
-<footer>
-    <p class="center">Desenvolvido por Emily da Silva Fitzlaff</p>
-</footer>
